7f4507cb7f326e28ac7b999c989322af1f1fb328
JAS0N HUANG <jas0nhuang@tuta.io>
2021-01-04 04:28:27 +0100

    change font

--- src/constants/GlobalStyle.js
+++ src/constants/GlobalStyle.js
@@ -2,10 +2,11 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   * {
+    @import url('https://fonts.googleapis.com/css2?family=Inconsolata&display=swap');
+    font-family: 'Inconsolata', monospace;
     margin: 0;
     text-align: center;
     box-sizing: border-box;
-    font-family: Courier,monospace;
   }
   h1 {
     font-size: 28px;
